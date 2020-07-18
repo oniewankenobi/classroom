ded210b540f298db590ba50d8cda9cb8b984971f
Sebastian Arthur Chua <sebastian.a.chua@gmail.com>
2020-07-19 01:42:22 +1000

    started with ar template from ole

--- classroom/lib/ARPage.dart
+++ classroom/lib/ARPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ARPage extends StatefulWidget {
   @override
@@ -7,15 +9,46 @@ class ARPage extends StatefulWidget {
 }
 
 class _ARPageState extends State<ARPage> {
+  ARKitController arkitController;
+  ARKitReferenceNode node;
+  String anchorId;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("AR Page"),
       ),
-      body: Center(
-        child: Text("AR PAGE"),
+      body: ARKitSceneView(
+        showFeaturePoints: true,
+        planeDetection: ARPlaneDetection.horizontal,
+        onARKitViewCreated: onARKitViewCreated,
       ),
     );
+  }
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+    this.arkitController.onAddNodeForAnchor = _handleAddAnchor;
+  }
+
+  void _handleAddAnchor(ARKitAnchor anchor) {
+    if (!(anchor is ARKitPlaneAnchor)) {
+      return;
+    }
+    _addPlane(arkitController, anchor);
+  }
+
+  void _addPlane(ARKitController controller, ARKitPlaneAnchor anchor) {
+    anchorId = anchor.identifier;
+    if (node != null) {
+      controller.remove(node.name);
+    }
+    node = ARKitReferenceNode(
+      url: 'Models.scnassets/eevie.dae',
+      position: vector.Vector3(0, 0, 0),
+      scale: vector.Vector3(0.002, 0.002, 0.002),
+    );
+    controller.add(node, parentNodeName: anchor.nodeName);
   }
 }
