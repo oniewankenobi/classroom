7ff70f846d509ef03b7a2f591ac2d50197506c81
Selina Chua <selina.a.chua@gmail.com>
2020-07-18 19:20:19 +1000

    Changed ui for home page.

--- classroom/lib/HomePage.dart
+++ classroom/lib/HomePage.dart
@@ -32,16 +32,95 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Row(
-        children: <Widget>[
-          _buildClassCard(context, "Chemistry"),
-          _buildClassCard(context, "Biology"),
-        ],
-      ),
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 30,
+          left: 30,
+          right: 30,
+        ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 50),
+              Text(
+                "Hello,",
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Chris Tine",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              Divider(
+                color: Colors.grey,
+              ),
+              SizedBox(height: 20),
+              _buildScienceSection(context),
+            ],
+          ),
+        ),
+      );
+  }
+
+  Widget _buildScienceSection(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "Science",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 10),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: <Widget>[
+              _buildClassCard(
+                context,
+                "Chemistry",
+                Icon(
+                  Icons.bubble_chart,
+                  color: Colors.green,
+                  size: 70,
+                ),
+              ),
+              SizedBox(width: 10),
+              _buildClassCard(
+                context,
+                "Biology",
+                Icon(
+                  Icons.school,
+                  color: Colors.blue,
+                  size: 70,
+                ),
+              ),
+              SizedBox(width: 10),
+              _buildClassCard(
+                context,
+                "Physics",
+                Icon(
+                  Icons.smartphone,
+                  color: Colors.red,
+                  size: 70,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
-  Widget _buildClassCard(BuildContext context, String title) {
+  Widget _buildClassCard(BuildContext context, String title, Icon icon) {
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -51,13 +130,34 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         );
       },
-      child: Card(
-        color: Colors.white,
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Text(title),
+      child: Container(
+        width: 150,
+        height: 150,
+        child: Card(
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          semanticContainer: true,
+          color: Colors.white.withOpacity(0.8),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                icon,
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
     );
   }
 }
